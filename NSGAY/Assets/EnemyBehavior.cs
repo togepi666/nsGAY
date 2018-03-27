da79bfb30c501b0e8163c6432283a1e5be961c97
LAPTOP-KFHBV4V2\Edgar <egr274@nyu.edu>
2018-03-27 11:54:14 -0400

    Remade script for enemies.

---
+++ NSGAY/Assets/EnemyBehavior.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.WSA;
+
+public class EnemyBehavior : MonoBehaviour
+{
+	public GameObject mainCharacter;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.position = Vector3.MoveTowards(transform.position, mainCharacter.transform.position, 0.05f);
+	}
+
+	void OnMouseClick()
+	{
+		Destroy(gameObject);
+	}
+}
