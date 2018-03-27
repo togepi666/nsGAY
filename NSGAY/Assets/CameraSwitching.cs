285d7d26d35af15bf164d9d4ecb325dd8988e026
Duanger <aaronshenglu@gmail.com>
2018-03-27 11:16:18 -0400

    Rebuilt RendTexture thing in enviormental greybox

--- NSGAY/Assets/CameraSwitching.cs
+++ NSGAY/Assets/CameraSwitching.cs
@@ -2,15 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraSwitching : MonoBehaviour {
-
-	// Use this for initialization
+public class CameraSwitching : MonoBehaviour
+{
+	public int CurrentMaterialIndex = 0;
+	public Renderer MainScreen, Screen2, Screen3;
+	public Material[] RenderTextureMaterial = new Material[3];
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetKeyDown(","))
+		{
+			CurrentMaterialIndex--;
+		}
+
+		if (Input.GetKeyDown("."))
+		{
+			CurrentMaterialIndex++;
+		}
+		switch (CurrentMaterialIndex)
+		{
+			default:
+				MainScreen.material = RenderTextureMaterial[0];
+				Screen2.material = RenderTextureMaterial[1];
+				Screen3.material = RenderTextureMaterial[2];
+				break;
+			case 1:
+				MainScreen.material = RenderTextureMaterial[1];
+				Screen2.material = RenderTextureMaterial[2];
+				Screen3.material = RenderTextureMaterial[0];
+				break;
+			case 2:
+				MainScreen.material = RenderTextureMaterial[2];
+				Screen2.material = RenderTextureMaterial[0];
+				Screen3.material = RenderTextureMaterial[1];
+				break;
+		}
+
+		if (CurrentMaterialIndex <= -1)
+		{
+			CurrentMaterialIndex = 2;
+		}
+
+		if (CurrentMaterialIndex >= 3)
+		{
+			CurrentMaterialIndex = 0;
+		}
 	}
 }
