894827d37703f09df23e1c2f83983ad4129a9abd
LAPTOP-KFHBV4V2\Edgar <egr274@nyu.edu>
2018-03-29 10:00:20 -0400

    Starting GeneralThings. Not finished

---
+++ NSGAY/Assets/GeneralThings.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using Random = System.Random;
+
+public class GeneralThings : MonoBehaviour
+{
+
+	public GameObject enemies;
+
+	public GameObject mainCharacter;
+	public GameObject spawnArea1;
+	public GameObject spawnArea2;
+	public GameObject spawnArea3;
+	
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void SpawnEnemy()
+	{
+		int ran = (int)(UnityEngine.Random.RandomRange(0, 3)+1);
+		switch (ran)
+		{
+				case 1: Instantiate(enemies,spawnArea1.transform.position,Quaternion.identity,null);
+					break;
+				case 2: Instantiate(enemies,spawnArea2.transform.position,Quaternion.identity,null);
+					break;
+				case 3: Instantiate(enemies,spawnArea3.transform.position,Quaternion.identity,null);
+					break;
+
+		}
+	}
+}
