ac5d21bc17a691ac338b814a0cfae8592835e37b
LAPTOP-KFHBV4V2\Edgar <egr274@nyu.edu>
2018-03-29 12:18:58 -0400

    Added girl and spawn points. UPdated enemy and spawn enemy script

--- NSGAY/Assets/EnemyBehavior.cs
+++ NSGAY/Assets/EnemyBehavior.cs
@@ -7,18 +7,36 @@ public class EnemyBehavior : MonoBehaviour
 {
 	public GameObject mainCharacter;
 	// Use this for initialization
+
+	public bool alive = true;
+
+	public float speed = 0;
+	// Use this for initialization
 	void Start () {
-		
+		mainCharacter = GameObject.Find("Girl");
+		speed = Random.RandomRange(1, 1.5f);
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		transform.position = Vector3.MoveTowards(transform.position, mainCharacter.transform.position, 0.05f);
+		if (alive)
+		{
+			transform.position = Vector3.MoveTowards(transform.position, mainCharacter.transform.position, 0.05f * speed);
+		}
 	}
 
 	void OnMouseClick()
 	{
 		Destroy(gameObject);
+	}
+
+	private void OnCollisionEnter(Collision other)
+	{
+		if (other.gameObject.CompareTag("Bullet"))
+		{
+			GetComponent<Rigidbody>().useGravity = true;
+			alive = false;
+		}
 	}
 }
---
+++ NSGAY/Assets/GeneralThings.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneralThings : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject enemies;
+
+	public GameObject mainCharacter;
+	public GameObject spawnArea1;
+	public GameObject spawnArea2;
+	public GameObject spawnArea3;
+
+	public float currentTime;
+	void Update()
+	{
+		currentTime += Time.deltaTime;
+		if (currentTime > 3){
+			int ran = (int)(Random.RandomRange(0, 3)+1);
+
+			SpawnEnemy(ran);
+			currentTime = 0;
+		}
+	}
+
+
+	public void SpawnEnemy(int ran)
+	{
+		
+		switch (ran)
+		{
+			case 1: Instantiate(enemies,spawnArea1.transform.position,Quaternion.identity,null);
+				break;
+			case 2: Instantiate(enemies,spawnArea2.transform.position,Quaternion.identity,null);
+				break;
+			case 3: Instantiate(enemies,spawnArea3.transform.position,Quaternion.identity,null);
+				break;
+
+		}
+	}
+}
