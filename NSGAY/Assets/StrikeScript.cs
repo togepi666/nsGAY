02e7ddcf06ef85c50f7e48da96d2d2310d3d07dd
gc-checkout-PC\gc-checkout <erk347@nyu.edu>
2018-04-03 11:43:57 -0400

    turned enemies red started strike script

---
+++ NSGAY/Assets/StrikeScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StrikeScript : MonoBehaviour {
+    public int strikes = 3;
+    public bool gameOver = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (strikes <= 0)
+        {
+            gameOver = true;
+        }
+	}
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            strikes--;
+        }
+    }
+
+}
