b91f9a321be2ab81702f83ca9d265ae9194c0758
Duanger <aaronshenglu@gmail.com>
2018-04-04 01:15:29 -0400

    Ugh my line of sight code only works partially....., something about the ray casting is fucked up

--- NSGAY/Assets/girlGaze.cs
+++ NSGAY/Assets/girlGaze.cs
@@ -1,71 +1,151 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEditor.Graphs;
 using UnityEngine;
 
 public class girlGaze : MonoBehaviour
 {
+	public float GirlFOV = 110f;
+	public float GirlGazeRadius;
+	[Range(0,360)]
+	public float GirlGazeAngle;
+	public LayerMask[] deviceLayerMask = new LayerMask[3];
+	public LayerMask CameraMasks;
+	public List<Transform> VisibilityList = new List<Transform>();
+	public Transform[] CameraTransform = new Transform[3];
 
-	public Transform Girl;
 
-	private bool _compCamTriggered, _phoneCamTriggered, _echoCamTriggered;
 	private CameraSwitching _camSwitch;
+	private float _currentRayAngle;
 	private GameObject _camController;
+	private Vector3 _gazeDirection;
 	private void Start()
 	{
 		_camController = GameObject.FindGameObjectWithTag("CameraController");
 		_camSwitch = _camController.GetComponent<CameraSwitching>();
+		//StartCoroutine(FindTarget());
 	}
 
-	private void FixedUpdate()
+	void FixedUpdate()
 	{
-		LayerMask compCamLayerMask = LayerMask.GetMask("Computer");
-		LayerMask phoneCamLayerMask = LayerMask.GetMask("Phone");
-		LayerMask echoCamLayerMask = LayerMask.GetMask("Echo");
-		Ray girlRay = new Ray(transform.position,Girl.position);
-		RaycastHit compOnHit,phoneOnHit,echoOnHit;
-		if (Physics.Raycast(girlRay, out compOnHit, compCamLayerMask))
+		DetectCamera();
+	}
+	/*IEnumerator FindTarget()
+	{
+		yield return  new WaitForSeconds(0.2f);
+		DetectCamera();
+	}*/
+	private void DetectCamera()
+	{
+		//VisibilityList.Clear();
+		Collider[] visbleCameras =
+			Physics.OverlapSphere(transform.position, GirlGazeRadius, CameraMasks);
+		for (int i = 0; i < visbleCameras.Length; i++)
 		{
-			if (_camSwitch.CurrentMaterialIndex == 0)
+			Transform cameraPos = visbleCameras[i].transform;
+			_gazeDirection = (cameraPos.position - transform.position).normalized;
+			if (Vector3.Angle(transform.forward, _gazeDirection) < GirlGazeAngle / 2)
 			{
-				//GameManager class that contains bool that tracks the CompCam hit as true
-			}
-		}
-		if (!Physics.Raycast(girlRay, out compOnHit, compCamLayerMask))
-		{
-			if (_camSwitch.CurrentMaterialIndex == 0)
-			{
-				//GameManager class that contains bool that tracks the CompCam hit as false
-			}
-		}
-		if (Physics.Raycast(girlRay, out phoneOnHit, phoneCamLayerMask))
-		{
-			if (_camSwitch.CurrentMaterialIndex == 1)
-			{
-				//GameManager class that contains bool that tracks the PhoneCam hit as true
-			}
-		}
-		if (!Physics.Raycast(girlRay, out phoneOnHit, phoneCamLayerMask))
-		{
-			if (_camSwitch.CurrentMaterialIndex == 1)
-			{
-                //GameManager class that contains bool that tracks the PhoneCam hit as false
-			}
-		}
-		if (Physics.Raycast(girlRay, out echoOnHit, echoCamLayerMask))
-		{
-			if (_camSwitch.CurrentMaterialIndex == 2)
-			{
-				//GameManager class that contains bool that tracks the EchoCam hit as true
-			}
-		}
-		if (!Physics.Raycast(girlRay, out echoOnHit, echoCamLayerMask))
-		{
-			if (_camSwitch.CurrentMaterialIndex == 2)
-			{
-				//GameManager class that contains bool that tracks the EchoCam hit as false
-			}
+				float rayDistance = Vector3.Distance(transform.position, cameraPos.position);
+				if (Physics.Raycast(transform.position, _gazeDirection, rayDistance, CameraMasks))
+				{
+					VisibilityList.Add(cameraPos);
+				}
 		}
 		
+			/*else
+			{
+				_camSwitch.DeviceTriggered[_camSwitch.CurrentMaterialIndex] = false;
+			}*/
+		}
+
+
+		/*if (Physics.Raycast(girlRay, out compOnHit,rayDist, compCamLayerMask))
+		{
+			if (_camSwitch.CurrentMaterialIndex == 0)
+			{
+				_camSwitch.CompTriggered = true;
+			}
+		}
+		if (!Physics.Raycast(girlRay, out compOnHit,rayDist, compCamLayerMask))
+		{
+			if (_camSwitch.CurrentMaterialIndex == 0)
+			{
+				_camSwitch.CompTriggered = false;
+			}
+		}
+		if (Physics.Raycast(girlRay, out phoneOnHit,rayDist, phoneCamLayerMask))
+		{
+	
+			if (_camSwitch.CurrentMaterialIndex == 1)
+			{
+				_camSwitch.PhoneTriggered = true;
+			}
+		}
+		if (!Physics.Raycast(girlRay, out phoneOnHit,rayDist, phoneCamLayerMask))
+		{
+			if (_camSwitch.CurrentMaterialIndex == 1)
+			{
+				_camSwitch.PhoneTriggered = false;
+			}
+		}
+		if (Physics.Raycast(girlRay, out echoOnHit,rayDist, echoCamLayerMask))
+		{
+			if (_camSwitch.CurrentMaterialIndex == 2)
+			{
+				_camSwitch.EchoTriggered = true;
+			}
+		}
+		if (!Physics.Raycast(girlRay, out echoOnHit,rayDist, echoCamLayerMask))
+		{
+			if (_camSwitch.CurrentMaterialIndex == 2)
+			{
+				_camSwitch.EchoTriggered = false;
+			}
+		}*/
+	}
+
+	private void OnDrawGizmos()
+	{
+		Gizmos.color = Color.green;
+		Gizmos.DrawWireSphere(transform.position, GirlGazeRadius);
+	}
+
+
+	public Vector3 FindCurrentAngle(float gazeAngle,bool isAngleGlobal)
+	{
+		if (!isAngleGlobal)
+		{
+			gazeAngle += transform.eulerAngles.y;
+		}
+		return new Vector3(Mathf.Sin(gazeAngle*Mathf.Deg2Rad),0,Mathf.Cos(gazeAngle*Mathf.Deg2Rad));
+	}
+}
+[CustomEditor(typeof(girlGaze))]
+public class drawSight : Editor
+{
+	private GameObject _camObj;
+	private CameraSwitching _cam;
+
+	private void OnEnable()
+	{
+		_camObj = GameObject.FindGameObjectWithTag("CameraController");
+		_cam = _camObj.GetComponent<CameraSwitching>();
+	}
+	private void OnSceneGUI()
+	{
+		Handles.color = Color.magenta;
+		girlGaze gazer = (girlGaze) target;
+		Handles.DrawWireArc(gazer.transform.position,Vector3.up,Vector3.forward, 360,gazer.GirlGazeRadius);
+		Vector3 viewLineA = gazer.FindCurrentAngle(-gazer.GirlGazeAngle / 2,false);
+		Vector3 viewLineB = gazer.FindCurrentAngle(gazer.GirlGazeAngle / 2,false);
+		Handles.DrawLine(gazer.transform.position,gazer.transform.position+viewLineA *gazer.GirlGazeRadius);
+		Handles.DrawLine(gazer.transform.position,gazer.transform.position+viewLineB *gazer.GirlGazeRadius);
+		Handles.color = Color.blue;
+		foreach (var camera in gazer.VisibilityList)
+		{
+			Handles.DrawLine(gazer.transform.position,camera.position);
+		}
 	}
 }
