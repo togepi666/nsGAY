6a8102760f52202456326f1486b0cc1d851822eb
LAPTOP-KFHBV4V2\Edgar <egr274@nyu.edu>
2018-04-04 00:07:06 -0400

    Girl AI on girl. She doesnt work well yet.

--- NSGAY/Assets/girlAI.cs
+++ NSGAY/Assets/girlAI.cs
@@ -8,6 +8,7 @@ public class girlAI : MonoBehaviour
 	private float girlTurn;
 	private float girlMotion;
 	Vector3 girlRotation;
+	public float currentTime = 0;
 
 	// Use this for initialization
 	void Start ()
@@ -20,19 +21,28 @@ public class girlAI : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
+		currentTime+=Time.deltaTime;
+		if (currentTime > 3)
+		{
+			girlTurn = Random.Range(-100f, 100f);
+			currentTime = 0;
+			girlRotation = new Vector3(0f, girlTurn, 0f);
+			
 
-		girlTurn = Random.Range(-180f, 180f);
-		girlRotation = new Vector3(0f, girlTurn, 0f);
-		
+		}
+		GetComponent<Rigidbody>().AddRelativeTorque(girlRotation, ForceMode.Force);
+
 		GetComponent<Rigidbody>().AddRelativeForce(new Vector3(0f,0f,girlMotion), ForceMode.Force);
 
+		//girlTurn = Random.Range(-180f, 180f);
+		
+		
 	}
 
 	void OnCollisionEnter(Collision turn)
 	{
 		if (turn.gameObject)
 		{
-			GetComponent<Rigidbody>().AddRelativeTorque(girlRotation, ForceMode.Force);
 		}
 	}
 }
