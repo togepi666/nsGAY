db33cb951da6b4a254f106cbc8c1415832d3452f
gc-checkout-PC\gc-checkout <erk347@nyu.edu>
2018-04-10 12:16:25 -0400

    added menu screen with UI buttons

---
+++ NSGAY/Assets/menuScreen.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class menuScreen : MonoBehaviour {
+
+    public Button b;
+
+	// Use this for initialization
+	void Start () {
+        b.onClick.AddListener(startGame);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    public void startGame()
+    {
+        Debug.Log("Clicked button");
+        SceneManager.LoadScene(0);
+    }
+}
