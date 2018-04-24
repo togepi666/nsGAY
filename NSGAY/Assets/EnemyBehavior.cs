3d9107d9f54ab981335b86bac080fe49fb626c19
gc-checkout-PC\gc-checkout <erk347@nyu.edu>
2018-04-24 11:34:42 -0400

    added scoring script/wrote raycast script but commented it out

--- NSGAY/Assets/EnemyBehavior.cs
+++ NSGAY/Assets/EnemyBehavior.cs
@@ -78,4 +78,5 @@ public class EnemyBehavior : MonoBehaviour
             mat.color = afterHit;
 		}
 	}
+    
 }
---
+++ NSGAY/Assets/ScoringScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoringScript : MonoBehaviour {
+    public int score;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GetComponent<Text>().text = "Score: " + score;
+    }
+}
